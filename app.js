f35e403aff2dfad334af4b4fe1bf8d1df36ff729
NimansalaBandara <nimansaladamayanthibandara@gmail.com>
2024-10-12 13:26:06 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+//console.log("");
+//D3uMi8NBQFJdM6Gq
+const express = require("express");
+const mongoose = require("mongoose");
+
+// Create an instance of express
+const app = express();
+
+// Middleware
+app.use("/", (req, res, next) => {
+    res.send("It Is Working");
+});
+
+// MongoDB connection
+mongoose.connect("mongodb+srv://Nimansala:D3uMi8NBQFJdM6Gq@cluster0.uyte8.mongodb.net/")
+    .then(() => console.log("Connected to MongoDB"))
+    .then(() => {
+        app.listen(5000, () => {
+            console.log("Server is running on port 5000");
+        });
+    })
+    .catch((err) => console.log(err));
